40a64e6685c8aafd746ad984759e1ffd2f143f8e
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-01-22 11:57:29 -0800

    Meets test requirements through HoneyMakerBee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
 var Bee = function() {
+  Grub.call();
+  this.age = 5;
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+Bee.prototype.color = 'yellow';
--- src/Grub.js
+++ src/Grub.js
@@ -1,9 +1,10 @@
 var Grub = function() {
   this.age = 0;
   this.color = 'pink';
-  this.food = 'jelly';
 };
 
 Grub.prototype.eat = function() {
   
-}
+};
+
+Grub.prototype.food = 'jelly';
